bf37d250948f23126e386652d6066f498532178d
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-10-31 14:49:00 +0100

    completed exercise one

---
+++ basic/1.1information/index.js
@@ -0,0 +1,8 @@
+const name = "Gaspar Saldana";
+const age = 29;
+const email = "gaspar.saldana.sandoval@gmail.com";
+const course = "Full stack developer";
+console.log("My name is: " + name);
+console.log("Age: " + age);
+console.log("My email is: " + email);
+console.log("Current course: " + course);

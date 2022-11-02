bb12c09fa6d4d0bb5f007c81f0d8e117faaa748b
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-11-02 16:04:25 +0100

    created a colorblind function

--- strings/2.4color-blind/index.js
+++ strings/2.4color-blind/index.js
@@ -3,3 +3,11 @@ const text =
 console.log(text);
 const result = text.replace("blue", "red").replace("yellow", "green");
 console.log(result);
+
+function myColorBlind(txt) {
+  return txt.replaceAll("blue", "red").replaceAll("yellow", "green");
+}
+
+console.log(
+  myColorBlind("i'm blue and yellow and the grass is so green with blue sky")
+);

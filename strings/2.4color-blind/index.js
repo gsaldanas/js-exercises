b5146485b57cb0fde53dcc572cf403c630ba1a85
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-11-02 15:59:13 +0100

    color blind text exercise

---
+++ strings/2.4color-blind/index.js
@@ -0,0 +1,5 @@
+const text =
+  "roses are blue, violets are red, grass is yellow, don't be so mean.";
+console.log(text);
+const result = text.replace("blue", "red").replace("yellow", "green");
+console.log(result);

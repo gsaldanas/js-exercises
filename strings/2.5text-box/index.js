fa37040dd331c1221736b60a3b02d2ff3ada91c7
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-11-02 16:18:50 +0100

    text box console exercise

---
+++ strings/2.5text-box/index.js
@@ -0,0 +1,8 @@
+const text = "Hello world ik ben Gaspar";
+const line = "-";
+const repeatLine = line.repeat(text.length);
+
+console.log(text.length);
+console.log("/" + repeatLine + "\\");
+console.log(`|${text}|`);
+console.log("\\" + repeatLine + "/");

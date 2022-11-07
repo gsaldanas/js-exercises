ec1c2cc8cc3ef69afd62cc3403ff1ea48ab2cebc
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-11-07 14:37:15 +0100

    array of vrienden

--- Arrays/5.3vrienden/index.js
+++ Arrays/5.3vrienden/index.js
@@ -1,2 +1,18 @@
 import rl from "readline-sync";
-let test = rl.question("is dit een test?");
+let vrienden = rl.question("Hoeveel vrienden wens je in te geven?" + " ");
+vrienden = parseInt(vrienden);
+let arrayVrienden = [];
+//array vrienden vullen
+for (let i = 1; i <= vrienden; i++) {
+  let naamVriend = rl.question("Geef vriend " + i + " in ");
+  arrayVrienden.push(naamVriend);
+}
+
+//array printen
+for (let i = 0; i < arrayVrienden.length; i++) {
+  console.log("- " + arrayVrienden[i]);
+}
+//for of tryout
+for (const naam of arrayVrienden) {
+  console.log(naam);
+}

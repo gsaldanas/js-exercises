6ec3afec62d8f7baf58cf9a19e02cb386d70c68b
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-11-07 15:16:11 +0100

    fixed bereken percentage gemiddelde

--- Arrays/5.4punten/index.js
+++ Arrays/5.4punten/index.js
@@ -1,12 +1,14 @@
-//array maken
+//variables aanmaken
 const grades = [16, 12, 16, 7, 17, 14, 9, 8, 18, 12];
 const aantalVakken = grades.length;
 let average = 0;
 let percentage = 0;
 let sumGrades = 0;
+const maxScore = 20;
+const geslaagd = 10;
 //fails checken met filter
 let fails = grades.filter(function (e) {
-  return e <= 10;
+  return e < geslaagd;
 });
 //average sum
 for (const grade of grades) {
@@ -15,8 +17,8 @@ for (const grade of grades) {
 //bereken average
 average = sumGrades / aantalVakken;
 
-//bereken percentage
-percentage = ((aantalVakken - fails.length) * 100) / aantalVakken;
+//het gemiddelde uitgedrukt in percentage van elk behaald score
+percentage = (average * 100) / maxScore;
 
 //ouput
 console.log("Fails: " + fails.length);
